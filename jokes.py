a9d7cc502eff75b6a1afe03ef736cf0afce28040
Ludmila39 <ludaandrej@gmail.com>
2022-04-11 15:31:57 +0300

    feat:add function

--- jokes.py
+++ jokes.py
@@ -2,5 +2,16 @@ import requests
 from bs4 import BeautifulSoup as b
 
 URL = 'https://www.anekdot.ru/last/good/'
-r = requests.get(URL)
-print(r.status_code)  #отвечает ли сервер
+
+def parser(url):
+    r = requests.get(url)
+#отвечает ли сервер (200-good)
+# print(r.status_code)
+
+    soup = b(r.text, "html.parser")
+#выкачиваем коллекцию анекдотов с сайта
+
+    anekdots = soup.find_all('div', class_='text')  #текст будет с тэгами
+    return [c.text for c in anekdots]   #очищаем от тегов
+# clear_anekdots = [c.text for c in anekdots]   #очищаем от тегов
+# print(clear_anekdots)
