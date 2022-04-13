aa68d7cf7e4c0db58cb700df5410158698bfa670
Ludmila39 <ludaandrej@gmail.com>
2022-04-13 15:12:16 +0300

    feat:add bot

--- jokes.py
+++ jokes.py
@@ -1,17 +1,39 @@
 import requests
+import random
+import telebot
 from bs4 import BeautifulSoup as b
 
+
+
+
 URL = 'https://www.anekdot.ru/last/good/'
+API_KEY = '5221485540:AAGocMsQXlj6vxIpoLL3e3NutRYWwDZsg8U'
+
 
 def parser(url):
     r = requests.get(url)
-#отвечает ли сервер (200-good)
-# print(r.status_code)
+    # отвечает ли сервер (200-good)
+    # print(r.status_code)
 
     soup = b(r.text, "html.parser")
-#выкачиваем коллекцию анекдотов с сайта
+    # выкачиваем коллекцию анекдотов с сайта
 
-    anekdots = soup.find_all('div', class_='text')  #текст будет с тэгами
-    return [c.text for c in anekdots]   #очищаем от тегов
+    anekdots = soup.find_all('div', class_='text')  # текст будет с тэгами
+    return [c.text for c in anekdots]  # очищаем от тегов
+
+
 # clear_anekdots = [c.text for c in anekdots]   #очищаем от тегов
 # print(clear_anekdots)
+
+list_of_jokes = parser(URL)
+random.shuffle(list_of_jokes)
+
+bot = telebot.TeleBot(API_KEY)
+@bot.message_handler(commands =['go'])
+
+def hello (message):
+    bot.send_message(message.chat.id, 'Hello, input another number to laugh!')
+
+bot.polling()
+
+
